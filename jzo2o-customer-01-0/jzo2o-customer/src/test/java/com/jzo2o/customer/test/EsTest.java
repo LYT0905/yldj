708b9ef4b0bc173e40da2f491713b59aabaf7f4e
a12bb <2713204748@qq.com>
2024-04-09 20:59:13 +0800

    init: customer-初始化

--- jzo2o-customer-01-0/jzo2o-customer/src/test/java/com/jzo2o/customer/test/EsTest.java
+++ jzo2o-customer-01-0/jzo2o-customer/src/test/java/com/jzo2o/customer/test/EsTest.java
@@ -5,7 +5,6 @@ import com.jzo2o.common.utils.BeanUtils;
 import com.jzo2o.customer.model.domain.ServeProviderInfo;
 import com.jzo2o.customer.model.domain.ServeProviderSync;
 import com.jzo2o.customer.service.IServeProviderSyncService;
-import com.jzo2o.es.core.ElasticSearchTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,25 +17,25 @@ import java.util.List;
 @Slf4j
 public class EsTest {
 
-    @Resource
-    private ElasticSearchTemplate elasticSearchTemplate;
-
-    @Resource
-    private IServeProviderSyncService serveProviderSyncService;
-
-    public EsTest() {
-    }
-
-    @Test
-    public void testBatchInsert() {
-
-        List<ServeProviderSync> data = serveProviderSyncService.list();
-        List<ServeProviderInfo> serveProviderInfos = BeanUtils.copyToList(data, ServeProviderInfo.class, (sync, info) -> {
-            info.setLocation(new Location(sync.getLon(), sync.getLat()));
-        });
-
-
-        Boolean batchInsertResult = elasticSearchTemplate.opsForDoc().batchInsert("serve_provider_info_1", serveProviderInfos);
-        log.info("batchInsertResult:{}", batchInsertResult);
-    }
+//    @Resource
+//    private ElasticSearchTemplate elasticSearchTemplate;
+//
+//    @Resource
+//    private IServeProviderSyncService serveProviderSyncService;
+//
+//    public EsTest() {
+//    }
+//
+//    @Test
+//    public void testBatchInsert() {
+//
+//        List<ServeProviderSync> data = serveProviderSyncService.list();
+//        List<ServeProviderInfo> serveProviderInfos = BeanUtils.copyToList(data, ServeProviderInfo.class, (sync, info) -> {
+//            info.setLocation(new Location(sync.getLon(), sync.getLat()));
+//        });
+//
+//
+//        Boolean batchInsertResult = elasticSearchTemplate.opsForDoc().batchInsert("serve_provider_info_1", serveProviderInfos);
+//        log.info("batchInsertResult:{}", batchInsertResult);
+//    }
 }
